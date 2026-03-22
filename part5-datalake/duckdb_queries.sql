0ef5d19a889277b2071cf24dc22d19e450aba32f
arifvastari222-star <arifvastari222@gmail.com>
2026-03-22 20:12:01 +0530

    Updated duckdb and diagram

--- part5-datalake/duckdb_queries.sql
+++ part5-datalake/duckdb_queries.sql
@@ -1,5 +1,4 @@
 -- Q1: List all customers along with the total number of orders they have placed
-
 SELECT
     c.customer_id,
     c.name,
@@ -9,42 +8,39 @@ LEFT JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
 GROUP BY c.customer_id, c.name;
 
--- -- Q2: Find the top 3 customers by total order value
-
+-- Q2: Find the top 3 customers by total order value
 SELECT
     c.customer_id,
     c.name,
-    SUM(o.quantity * p.price) AS total_value
+    SUM(p.quantity * p.unit_price) AS total_value
 FROM read_csv_auto('../datasets/customers.csv') c
 JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
 JOIN read_parquet('../datasets/products.parquet') p
-ON o.product_id = p.product_id
+ON o.order_id = p.order_id
 GROUP BY c.customer_id, c.name
 ORDER BY total_value DESC
 LIMIT 3;
 
 -- Q3: List all products purchased by customers from Bangalore
-
-SELECT
-    c.name,
+SELECT DISTINCT
+    c.name AS customer_name,
     p.product_name
 FROM read_csv_auto('../datasets/customers.csv') c
 JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
 JOIN read_parquet('../datasets/products.parquet') p
-ON o.product_id = p.product_id
+ON o.order_id = p.order_id
 WHERE c.city = 'Bangalore';
 
--- Q4: Join customers, orders and products
-
+-- Q4: Join all three files to show: customer name, order date, product name, and quantity
 SELECT
     c.name AS customer_name,
     o.order_date,
     p.product_name,
-    o.quantity
+    p.quantity
 FROM read_csv_auto('../datasets/customers.csv') c
 JOIN read_json_auto('../datasets/orders.json') o
 ON c.customer_id = o.customer_id
 JOIN read_parquet('../datasets/products.parquet') p
-ON o.product_id = p.product_id;
+ON o.order_id = p.order_id;
